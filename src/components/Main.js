b6735901a482e079d9d6bae288f58ac30cec89aa
osnatkulyok <117373663+osnatkulyok@users.noreply.github.com>
2022-12-09 15:20:16 +0200

    comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,43 @@ export function Main({ items, onToggleAll }) {
   //
   function HandleToggleAll(event) {
     onToggleAll(event.target.checked)
+    //part 2 - let's set check & uncheck view
+    let inputList = document.querySelectorAll('.toggle')
+    console.log(event.target.checked)
+    //
+    for (let item of inputList) {
+      if (event.target.checked) {
+        const createCheckedAtt = document.createAttribute('checked')
+        item.setAttributeNode(createCheckedAtt)
+        console.log('yay')
+        //add line-through if the task is done
+        let taskStyle = document.querySelectorAll('.lineTrough')
+        console.log('here', taskStyle)
+        for (let task of taskStyle) {
+          task.style.color = 'green'
+          task.style.textDecoration = 'line-through'
+        }
+        //remove line-through if the task isn't done
+      } else {
+        item.removeAttribute('checked')
+        let taskStyle = document.querySelectorAll('.lineTrough')
+        for (let task of taskStyle) {
+          task.style.color = 'red'
+          task.style.textDecoration = 'none'
+        }
+
+        console.log('noo')
+      }
+    }
   }
 
   return (
-    <section class="main">
-      <input class="toggle-all" onChange={HandleToggleAll} type="checkbox" />
+    <section className="main">
+      <input
+        className="toggle-all"
+        onChange={HandleToggleAll}
+        type="checkbox"
+      />
       <ToDoList items={items} />
     </section>
   )
