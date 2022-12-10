88da7e47d5638cee32c8799166039b367f1485a1
osnatkulyok <117373663+osnatkulyok@users.noreply.github.com>
2022-12-10 10:11:07 +0200

    comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 // import React from 'react'
 
+import { render } from '@testing-library/react'
+
 export function Header(props) {
   //
   function HandleTaskInput(event) {
     if (event.key === 'Enter') {
-      console.log('hey')
       props.onAddItem(event.target.value)
+      console.log(event.target.value)
+      event.target.value = ''
     }
   }
 
   return (
-    <header class="header">
+    <header className="header">
       <h1>{props.title}</h1>
       <input
         className="new-todo"
