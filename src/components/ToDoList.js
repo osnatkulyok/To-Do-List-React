02ebc3b24a59ac4a1d63c5e61e6cf258ec4719b5
osnatkulyok <117373663+osnatkulyok@users.noreply.github.com>
2022-12-10 17:39:31 +0200

    comments

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,30 +3,36 @@
 export function ToDoList({ items, markAsCompleted, removeTodo, onToggleAll }) {
   //
   function HandleDestroyButton(event) {
+    // console.log(event)
+    // console.log(event.target)
+    // console.log(event.target.parentElement.innerText)
+    // for (let item of items) {
+    //   // console.log(item.title)
+    //   // console.log(item.completed)
+    //   if (item.title === event.target.parentElement.innerText) {
+    //     console.log(item.id, item.completed)
+    //     console.log(item.id, item.completed)
+    //   }
+    // }
+
     let counter = document.querySelector('strong').innerText
-    // console.log(counter)
     counter -= 1
     document.querySelector('strong').innerText = counter
-    // console.log(counter)
-    console.log(event.target.parentElement.innerHTML)
     event.target.parentElement.innerHTML = ''
   }
-  //
+
   const markAsCompleted1 = (event) => {
-    console.log('heyys')
+    // console.log('heyys')
 
     const createCheckedAtt = document.createAttribute('checked')
     event.target.setAttributeNode(createCheckedAtt)
-    console.log(event.target.target)
-    console.log()
-
-    // const createCheckedAtt = document.createAttribute('checked')
-    //     item.setAttributeNode(createCheckedAtt)
-    //     console.log('yay')
+    // console.log(event.target)
+    // console.log(items.indexOf(event.target.parentElement))
+    // console.log(items)
   }
   return (
     <ul className="todo-list">
-      {items.map((items) => (
+      {items.map((item) => (
         <li className="parent">
           <div className="view">
             <input
@@ -34,7 +40,7 @@ export function ToDoList({ items, markAsCompleted, removeTodo, onToggleAll }) {
               className="toggle"
               type="checkbox"
             />
-            <label className="lineTrough">{items.title}</label>
+            <label className="lineTrough">{item.title}</label>
             <button className="destroy" onClick={HandleDestroyButton} />
           </div>
           <input className="edit" />
